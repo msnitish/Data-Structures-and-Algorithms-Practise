9e76d9e1b3c05c50003c580eb3ad50454de1f2ed
Nitish Chandra <25774996+msnitish@users.noreply.github.com>
2019-08-09 20:28:25 +0530

    Create palindrome_list.cpp

---
+++ Linked Lists/palindrome_list.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+int Solution::lPalin(ListNode* A) {
+    ListNode* temp = A;
+    stack<int> s;
+    // We create a stack and push all these values into a stack.
+    // So, that when we read it again from the top, we read the reverse order.
+    while(temp!=NULL){
+        s.push(temp->val);
+        temp = temp->next;
+    }
+    //Now we check if the values of the top element in stack is same as the value we see
+    // in the linked list while we traverse it.
+    while(A!=NULL){
+        int i = s.top();
+        s.pop();
+        if(A->val != i){
+            return false;
+        }
+        A = A->next;
+    }
+    
+    return true;
+    
+}
