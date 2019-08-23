5d82468aa30e7793e8d22cb5a48f91d00447f7be
Nitish Chandra <25774996+msnitish@users.noreply.github.com>
2019-08-24 04:15:11 +0530

    Update palindrome_list.cpp

--- Linked Lists/palindrome_list.cpp
+++ Linked Lists/palindrome_list.cpp
@@ -6,28 +6,35 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
-
-// Interviewbit Problem
 int Solution::lPalin(ListNode* A) {
-    ListNode* temp = A;
-    stack<int> s;
-    // We create a stack and push all these values into a stack.
-    // So, that when we read it again from the top, we read the reverse order.
-    while(temp!=NULL){
-        s.push(temp->val);
-        temp = temp->next;
-    }
-    //Now we check if the values of the top element in stack is same as the value we see
-    // in the linked list while we traverse it.
-    while(A!=NULL){
-        int i = s.top();
-        s.pop();
-        if(A->val != i){
-            return false;
-        }
-        A = A->next;
-    }
+    // Method 1 : We will use stack data structure to find if the linked list is a palindrome.
     
-    return true;
+/*So, we first put all the elements in LL into the stack one by one. So, after filling the
+stack, the top element of the stack is the last element in linked list.
+
+We can again traverse the linked list from begin using another pointer to check for 
+each value if it is equal to the top value in the stack. By this way, we get to check 
+that the reversed linked list and normal LL have the same values which is what
+a palindrome is.
+
+*/
+if(!A) return true;
+ListNode* temp = A;
+stack<int> s;
+while(temp!=NULL){
+    s.push(temp->val);
+    temp = temp->next;
+}
+
+while(A!=NULL){
+    int i = s.top();
+    s.pop();
+    if(A->val != i) return false;
+    A = A->next;
+}
+return true;
+
+
+
     
 }
