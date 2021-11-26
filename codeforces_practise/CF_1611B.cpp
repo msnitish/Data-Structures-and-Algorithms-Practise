8c25bd4fefbc88f319ff6e7e3d4abfa556662e68
msnitish <msnitish@gmail.com>
2021-11-26 19:23:13 +0530

    CF Div2 B question - 11/26

---
+++ codeforces_practise/CF_1611B.cpp
@@ -0,0 +1,23 @@
+// C++ program to count total anagram
+// substring of a string
+#include <bits/stdc++.h>
+#include <algorithm>
+using namespace std;
+#define MAX_CHAR 26
+int main()
+{ 
+   
+freopen("input.txt", "r", stdin);
+freopen("output.txt", "w", stdout);
+   int t; cin >> t;
+while(t--){
+        int a, b;
+        cin >> a >> b;
+        int sum  = (a+b)/4;
+        int mini = min(a, b);
+        cout << min(mini, sum) << endl;
+
+}
+
+
+}
